2059cfca235f02b6207ff31d98712a6e129c3c70
desire627 <desireasiimwe24@gmail.com>
2024-10-22 12:13:46 +0300

    redone

--- factorial.js
+++ factorial.js
@@ -1,14 +1,9 @@
 export function factorial(x) {
-    if (x < 0) {
-      throw new RangeError('x must be a non-negative integer');
-    }
-    if (x === 0 || x === 1) {
-      return 1;
-    }
-    let result = 1;
-    for (let i = 2; i <= x; i++) {
-      result *= i;
-    }
-    return result;
+  if (x < 0) {
+    throw new RangeError('x must be a non-negative integer');
   }
-  
+  if (x === 0 || x === 1) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
